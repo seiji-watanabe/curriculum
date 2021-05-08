fb95e08bb0552ebd87835a41346c1dd7d8742a35
seiji-watanabe <83572937+seiji-watanabe@users.noreply.github.com>
2021-05-08 15:39:39 +0900

    Update Task1_4.java

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -41,11 +41,11 @@ public class Task1_4 {
         //      定数を使用して「 ログイン成功です 」と出力してください。
         if (name.equals(USER_NAME) && pass.equals(USER_PASSWORD){
           System.out.println(CONST_MSG_SUCCESS);
-        } else if (name == USER_NAME) {
+        } else if (name.equals(USER_NAME) {
           // ③ 「USER_NAME」の値のみ等しい場合。
           //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
           System.out.println(CONST_MSG_ERROR_PASS);
-        }else if (pass == USER_PASSWORD) {
+        }else if (pass.equals(USER_PASSWORD) {
           // ④ 「USER_PASSWORD」の値のみ等しい場合。
           //定数を使用して「 名前に誤りがあります。 」と出力してください。
           System.out.println(CONST_MSG_ERROR_NAME);
